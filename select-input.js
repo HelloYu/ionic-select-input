12d0e700634f921a9f9206b54cb9872dfc3cf7f8
Jeffrey Yu <jeffreycolbert@foxmail.com>
2015-12-31 11:17:53 +0800

    update code

--- select-input.js
+++ select-input.js
@@ -1,16 +1,37 @@
-angular.module('ionic-addon', ['ionic']).directive('selectInput', ['$ionicPopup', function($ionicPopup) {
-  return {
+(function() {
+	'use strict';
+	angular
+		.module('app.widgets')
+		/**
+		 * 
+		 * @class app.widgets.selectInput
+		 * 可输入选择框控件，封装ionic `$ionicPopup` 控件。  
+		 * 
+		 * 
+		 */
+		.directive('selectInput',selectInput);
+
+	selectInput.$inject = ['$ionicPopup'];
+	function selectInput($ionicPopup) {
+
+		var directive = {
     restric: 'E',
     scope: {
       currentInput: '=ngModel',
       selectOptions: '='
     },
     require: '?^ngModel',
-    template: '<div class="item-input item-icon-right"><input ng-model="currentInput"><i class="icon ion-android-arrow-dropdown "  ng-click="showOptions()"></i></div>',
-    link: function(scope, element, attrs) {
+    template: '<input ng-model="currentInput" style="border:0;"><i class="icon ion-android-arrow-dropdown "  ng-click="showOptions()"></i>',
+    link: link,
+ 		}
+ 		return directive;
+
+ 		function link(scope, element, attrs) {
       scope.options = {
         selected: ''
       }
+      console.info(scope);
+      console.info(element);
       scope.showOptions = function() {
         $ionicPopup.show({
           template: '<ion-radio ng-repeat="item in selectOptions" ng-model="options.selected" ng-value="item">{{item}}</ion-radio>',
@@ -28,5 +49,5 @@ angular.module('ionic-addon', ['ionic']).directive('selectInput', ['$ionicPopup'
         });
       }
     }
-  }
-}])
+  };
+})();
